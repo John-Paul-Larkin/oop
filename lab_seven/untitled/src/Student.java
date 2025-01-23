1b018bb43541921f7c817bccc29f76da33049ed0
John Paul Larkin <107807466+John-Paul-Larkin@users.noreply.github.com>
2025-01-23 20:53:56 +0000

    student

--- lab_seven/untitled/src/Student.java
+++ lab_seven/untitled/src/Student.java
@@ -1,3 +1,6 @@
+// John Paul Larkin
+// C00001754
+// OOP - Lab seven - 20/1/25
 
 public class Student {
     // Private global variables
@@ -5,36 +8,39 @@ public class Student {
     private int id;
     private String module;
 
-    // Constructor with 3 parameters
+    // Constructor 
     public Student(String mName, int mID, String mModule) {
         this.name = mName;
         this.id = mID;
         this.module = mModule;
     }
 
-    // Getter and Setter for 'name'
+    // Getter for 'name'
     public String getName() {
         return name;
     }
 
+    // Setter for 'name'
     public void setName(String name) {
         this.name = name;
     }
 
-    // Getter and Setter for 'id'
+    // Getter for 'id'
     public int getId() {
         return id;
     }
 
+    // Setter for 'id'
     public void setId(int id) {
         this.id = id;
     }
 
-    // Getter and Setter for 'module'
+    // Getter for 'module'  
     public String getModule() {
         return module;
     }
 
+    // Setter for 'module'
     public void setModule(String module) {
         this.module = module;
     }
