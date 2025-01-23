52f1261a7791ba2ae74517c0727378cc4937b74e
John Paul Larkin <107807466+John-Paul-Larkin@users.noreply.github.com>
2025-01-23 20:53:09 +0000

    item

--- lab_seven/untitled/src/Item.java
+++ lab_seven/untitled/src/Item.java
@@ -1,28 +1,34 @@
+// John Paul Larkin
+// C00001754
+// OOP - Lab seven - 20/1/25
+
 public class Item {
     // Private global variables
     private int price;
     private int stock;
 
-    // No-argument constructor
+    // Constructor with no parameters defaults price and stock to 0
     public Item() {
         this.price = 0;
         this.stock = 0;
     }
 
-    // Setters
+    // Setter for 'price'
     public void setPrice(int price) {
         this.price = price;
     }
 
+    // Setter for 'stock'
     public void setStock(int stock) {
         this.stock = stock;
     }
 
-    // Getters
+    // Getter for 'price'
     public int getPrice() {
         return price;
     }
 
+    // Getter for 'stock'
     public int getStock() {
         return stock;
     }
